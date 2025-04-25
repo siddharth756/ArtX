7f3cb1a7cb50737ad1b1024eacb5a13e43798caf
siddharth756 <siddharthsarvaiya56@gmail.com>
2025-04-25 18:04:06 +0530

    Component modified.

--- artx-frontend/components/Gallary.jsx
+++ artx-frontend/components/Gallary.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useDispatch, useSelector } from "react-redux"
 import { fetchImages, setGallary } from '../feature/imageSlice';
 
 function Gallary() {
+    const [fadeIn, setFadeIn] = useState(false)
     const gallary = useSelector((state) => state.images.gallary)
+    const loading = useSelector((state) => state.images.loading)
     const dispatch = useDispatch()
     const hasFetchedOnce = useRef(false)
 
@@ -12,9 +14,11 @@ function Gallary() {
         if (sessionImages) {
             const parsed = JSON.parse(sessionImages)
             dispatch(setGallary(parsed))
+            setFadeIn(true)
         } else if (!hasFetchedOnce.current && (!gallary || gallary === undefined || gallary.length === 0)) {
             hasFetchedOnce.current = true
-            dispatch(fetchImages()).then(res => {sessionStorage.setItem("gallary", JSON.stringify(res.payload))})
+            dispatch(fetchImages()).then(res => { sessionStorage.setItem("gallary", JSON.stringify(res.payload)) })
+            setTimeout(() => setFadeIn(true), 100)
         }
     }, [])
 
@@ -26,28 +30,43 @@ function Gallary() {
                 </span>
             </h1>
 
-            <div className="grid grid-cols-2 md:grid-cols-4 gap-4 max-w-7xl mx-auto">
-                {gallary && gallary.length !== 0 && gallary.map((img, idx) => (
-                    <div
-                        key={idx}
-                        className="relative w-full mb-4 rounded-lg overflow-hidden group"
-                    >
-                        {/* Image with darkening effect on hover */}
-                        <img
-                            src={img.imageUrl}
-                            alt={`Generated ${idx}`}
-                            className="w-full rounded-lg transition-all duration-300 group-hover:brightness-50"
-                        />
-
-                        {/* Stacked Info at Bottom on Hover */}
-                        <div className="absolute bottom-0 left-0 w-full p-4 bg-gradient-to-t from-black/90 to-transparent opacity-0 group-hover:opacity-100 transition-all text-white space-y-1">
-                            <h3 className="text-lg font-bold">Information</h3>
-                            <p className="text-sm"> 👤 User : <span className="font-medium">{img.username ?? "Unknown"}</span></p>
-                            <p className="text-sm"> 💬  Prompt :  <span className="italic">{img.prompt}</span></p>
+            {
+                loading ?
+                    <div className="mt-58 flex justify-center items-center">
+                        <div className="absolute flex space-x-1">
+                            <div className="w-2 h-8 bg-indigo-500 animate-pulse [animation-delay:-0.3s]" />
+                            <div className="w-2 h-8 bg-purple-500 animate-pulse [animation-delay:-0.15s]" />
+                            <div className="w-2 h-8 bg-blue-500 animate-pulse" />
                         </div>
                     </div>
-                ))}
-            </div>
+                    :
+                    (
+                        <div className={`grid grid-cols-2 md:grid-cols-4 gap-4 max-w-7xl mx-auto 
+                            transition-opacity duration-700 ease-in-out 
+                            ${fadeIn ? "opacity-100" : "opacity-0"}`}>
+                            {gallary && gallary.length !== 0 && gallary.map((img, idx) => (
+                                <div
+                                    key={idx}
+                                    className="relative w-full mb-4 rounded-lg overflow-hidden group"
+                                >
+                                    {/* Image with darkening effect on hover */}
+                                    <img
+                                        src={img.imageUrl}
+                                        alt={`Generated ${idx}`}
+                                        className="w-full rounded-lg transition-all duration-300 group-hover:brightness-50"
+                                    />
+
+                                    {/* Stacked Info at Bottom on Hover */}
+                                    <div className="absolute bottom-0 left-0 w-full p-4 bg-gradient-to-t from-black/90 to-transparent opacity-0 group-hover:opacity-100 transition-all text-white space-y-1">
+                                        <h3 className="text-lg font-bold">Information</h3>
+                                        <p className="text-sm"> 👤 User : <span className="font-medium">{img.username ?? "Unknown"}</span></p>
+                                        <p className="text-sm"> 💬  Prompt :  <span className="italic">{img.prompt}</span></p>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    )
+            }
         </div>
     )
 }
